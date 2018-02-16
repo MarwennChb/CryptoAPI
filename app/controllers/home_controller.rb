08caa8617f4a46c1316055f12611bb023673731b
marwenchb <marwenchb@gmail.com>
2018-02-16 10:26:10 +0100

    Version Finale sans pimpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
-  def index
-  end
+
+	def index
+
+		@price=nil
+		@currency=nil
+
+		if params[:search]
+			@currency = params_choice[:choice]
+		end
+
+		@currencies = CryptoPrice.new.cryptocurrencies 
+		
+		@price = CryptoPrice.new.perform(@currency)
+	end
+
+	private
+
+	def params_choice
+		params.require(:search).permit(:choice)
+	end
 end
--- app/services/crypto_price.rb
+++ app/services/crypto_price.rb
@@ -1,27 +1,43 @@
 require 'open-uri'
+require 'nokogiri'
+
 
 class CryptoPrice
 
-	def initialize(coin)
-
-		@coin = coin
-
-	end
 
 
-	def perform
-		
-		@page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
+	def initialize
+		@page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))  
 
 		@name_list = @page.css('a.currency-name-container') 
-		@price_list = @page.css('a.price') 
 
-
-		@ind = @name_list.index(@coin)
-		@coin_price = @price_list[@ind]
-
-		puts @coin_price
-
+		@price_list = @page.css('a.price')  
 	end
+
+	def perform(name)
+
+
+
+
+@tab = Hash.new 
+
+
+
+for i in 0 .. (@name_list.length-1)
+
+	@tab[@name_list[i].text] = @price_list[i]['data-usd']
+
+end
+
+return @tab[name]
+
+end
+
+def cryptocurrencies
+
+	@currencies = @name_list.map{ |i| i.text}
+
+end
+
 
 end
